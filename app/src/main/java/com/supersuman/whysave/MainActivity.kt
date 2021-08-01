7b887150cef53a866ef1e28f29d78d3b1f9aa604
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2021-08-01 14:28:12 +0530

    v1.2
    - Added a button/dialog to invert call logs
    - Added Updater library

--- app/src/main/java/com/supersuman/whysave/MainActivity.kt
+++ app/src/main/java/com/supersuman/whysave/MainActivity.kt
@@ -2,32 +2,59 @@ package com.supersuman.whysave
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
+import com.supersuman.githubapkupdater.Updater
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager
+    private lateinit var rootLayout: CoordinatorLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tabLayout : TabLayout = findViewById(R.id.tabLayout)
-        tabLayout.addTab(tabLayout.newTab().setText("Type Number"))
-        tabLayout.addTab(tabLayout.newTab().setText("Call Logs"))
-        val viewPager : ViewPager = findViewById(R.id.viewPager)
-        val adapter = PagerAdapter(supportFragmentManager)
-        viewPager.adapter = adapter
-        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                viewPager.currentItem = tab?.position!!
+        initViews()
 
+        thread {
+            val updater = Updater(this,"https://github.com/supersu-man/WhySave/releases/latest")
+            checkForUpdates(updater)
+        }
+    }
+
+
+
+    private fun initViews() {
+        rootLayout = findViewById(R.id.rootLayout)
+        viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+        tabLayout.setupWithViewPager(viewPager)
+        viewPager.adapter = PagerAdapter(supportFragmentManager)
+    }
+
+    private fun checkForUpdates(updater: Updater){
+        if (updater.isInternetConnection()){
+            updater.init()
+            updater.isNewUpdateAvailable {
+                Snackbar.make(rootLayout,"New Update Found",Snackbar.LENGTH_INDEFINITE).setAction("Download"){
+                    if (updater.hasPermissionsGranted()){
+                        updater.requestDownload()
+                    } else{
+                        updater.requestMyPermissions {
+                            updater.requestDownload()
+                        }
+                    }
+                }.show()
             }
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-        })
-
+        }else{
+            Snackbar.make(rootLayout,"Unable To Check For Updates",Snackbar.LENGTH_SHORT).show()
+        }
     }
 
 }
