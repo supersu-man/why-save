718607a4c4ce08bd82e2eb955c47cdf2124b82ac
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2022-07-26 14:47:56 +0530

    feat: added download app buttons, title and desc

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,8 @@
 const input = document.getElementById('input')
-const button = document.getElementById('button')
+const chatButton = document.getElementById('chatButton')
+const downloadButton1 = document.getElementById('downloadButton1')
+const downloadButton2 = document.getElementById('downloadButton2')
+
 const toggle = document.getElementById('themeSwitch')
 const body = document.body
 
@@ -21,12 +24,28 @@ toggle.addEventListener('change', () => {
     }
 })
 
-button.addEventListener('click', () => {
+const ar = ['bruhh', 'really?', 'stop', 'yo?', 'ufff', 'bro?', 'all good?']
+
+chatButton.addEventListener('click', () => {
     if (!isNumber(input.value)) {
+        const randint = getRndInteger(0, ar.length - 1)
+        alert(ar[randint])
         return
     }
-    window.open(`https://api.whatsapp.com/send/?phone=%2B91${input.value}&text&app_absent=0`, "_self")
+    window.open(`https://api.whatsapp.com/send/?phone=%2B91${input.value}&text&app_absent=0`, '_self')
 })
+
+downloadButton1.addEventListener('click', () => {
+    window.open('https://play.google.com/store/apps/details?id=com.supersuman.whysave', '_self')
+})
+
+downloadButton2.addEventListener('click', () => {
+    window.open('https://github.com/supersu-man/why-save/releases', '_self')
+})
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function isNumber(number) {
     return number.length == 10 && !isNaN(number) && !isNaN(parseFloat(number))
