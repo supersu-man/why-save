bd8f4aecbff3c6d69033666ed7306dee961c6ca3
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2022-12-10 05:54:09 +0530

    fix: call logs (added library)

--- app/src/main/java/com/supersuman/whysave/CallLogsFragment.kt
+++ app/src/main/java/com/supersuman/whysave/CallLogsFragment.kt
@@ -5,117 +5,66 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
-import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
-import android.provider.CallLog
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.textfield.TextInputEditText
-import java.util.*
+import com.supersuman.whysave.databinding.FragmentCallLogsBinding
+import com.wickerlabs.logmanager.LogsManager
+import kotlin.concurrent.thread
 
 
 class CallLogsFragment : Fragment() {
 
-    private val callsLogsArray : MutableList<CallData> = mutableListOf()
-    private lateinit var recyclerView : RecyclerView
-    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
-    private lateinit var textView: TextView
-    private lateinit var phonenumberEditText: TextInputEditText
-    private lateinit var floatingActionButton: FloatingActionButton
+    private lateinit var binding: FragmentCallLogsBinding
     private lateinit var sharedPref: SharedPreferences
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentCallLogsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isEmpty()) return
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            getSetCallLogs()
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        initViews()
-        initListeners()
+        sharedPref = requireActivity().getSharedPreferences("WhySave", Context.MODE_PRIVATE)
+        binding.recyclerView.adapter = CallLogsRecyclerViewAdapter(mutableListOf())
         setupRefresh()
-
-        if (isPermissionPresent()){
-            getSetCallsLog()
-        }else{
-            requestPermission()
+        if (isPermissionsPresent()) {
+            getSetCallLogs()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_CONTACTS), 100)
         }
     }
 
-    private fun initListeners() {
-        floatingActionButton.setOnClickListener {
-            MaterialAlertDialogBuilder(this.requireContext())
-                .setTitle("Reverse list?")
-                .setMessage("This will reverse the call list")
-                .setPositiveButton("Yes"){ _, _ ->
-                    if (sharedPref.contains("ReverseLayout")){
-                        val linearManager =LinearLayoutManager(requireActivity().applicationContext,LinearLayoutManager.VERTICAL,false)
-                        linearManager.stackFromEnd = false
-                        recyclerView.layoutManager = linearManager
-                        sharedPref.edit().remove("ReverseLayout").apply()
-                    } else{
-                        val linearManager =LinearLayoutManager(requireActivity().applicationContext,LinearLayoutManager.VERTICAL,true)
-                        linearManager.stackFromEnd = true
-                        recyclerView.layoutManager = linearManager
-                        sharedPref.edit().putBoolean("ReverseLayout",true).apply()
-                    }
-                }
-                .setNegativeButton("No"){ _, _ ->
-                }
-                .show()
+    private fun getSetCallLogs() = thread {
+        val logsManager = LogsManager(context)
+        val callLogs = logsManager.getLogs(LogsManager.ALL_CALLS)
+        activity?.runOnUiThread {
+            binding.recyclerView.adapter = CallLogsRecyclerViewAdapter(callLogs)
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when(requestCode){
-            100 -> {
-                if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    getSetCallsLog()
-                }
-            }
-        }
+    private fun isPermissionsPresent(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(), Manifest.permission.READ_CALL_LOG
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            requireContext(), Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun initViews() {
-        sharedPref = requireActivity().getSharedPreferences(
-            "WhySave", Context.MODE_PRIVATE)
-        phonenumberEditText = requireActivity().findViewById(R.id.phonenumberEditText)
-        swipeRefreshLayout = requireActivity().findViewById(R.id.swipeRefreshLayout)
-        recyclerView = requireActivity().findViewById(R.id.recyclerView)
-        if (!sharedPref.contains("ReverseLayout")){
-            val linearManager =LinearLayoutManager(requireActivity().applicationContext,LinearLayoutManager.VERTICAL,false)
-            linearManager.stackFromEnd = false
-            recyclerView.layoutManager = linearManager
-        } else{
-            val linearManager =LinearLayoutManager(requireActivity().applicationContext,LinearLayoutManager.VERTICAL,true)
-            linearManager.stackFromEnd = true
-            recyclerView.layoutManager = linearManager
-        }
-        recyclerView.adapter = CallLogsRecyclerViewAdapter(callsLogsArray,phonenumberEditText)
-        textView = requireActivity().findViewById(R.id.warningTextView)
-        floatingActionButton = requireActivity().findViewById(R.id.floatingButton)
-    }
-
-    private fun requestPermission() {
-        requestPermissions(arrayOf(Manifest.permission.READ_CALL_LOG),100)
-    }
-
-    private fun isPermissionPresent(): Boolean {
-        return ContextCompat.checkSelfPermission(activity?.applicationContext!!, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun openPermissionPage(){
+    private fun openPermissionPage() {
         val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
         val uri = Uri.fromParts("package", requireActivity().packageName, null)
         intent.data = uri
@@ -123,83 +72,15 @@ class CallLogsFragment : Fragment() {
     }
 
     private fun setupRefresh() {
-        swipeRefreshLayout.setOnRefreshListener {
-            if (!isPermissionPresent()){
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            if (isPermissionsPresent()) {
+                getSetCallLogs()
+                Toast.makeText(requireActivity(), "Log refreshed", Toast.LENGTH_SHORT).show()
+            } else {
                 openPermissionPage()
-            } else{
-                getSetCallsLog()
-                Toast.makeText(requireActivity(), "Log refreshed",Toast.LENGTH_SHORT).show()
             }
-            swipeRefreshLayout.isRefreshing = false
+            binding.swipeRefreshLayout.isRefreshing = false
         }
     }
 
-    private fun getcallCodes(i : Int): String {
-        when(i){
-            1->return "Incoming Call"
-            2 ->return "Outgoing Call"
-            3 ->return "Missed Call"
-            4 ->return "Voicemail"
-            5->return "Declined Call"
-            6->return "Blocked Call"
-            7->return "Answered Externally"
-        }
-        return ""
-    }
-
-    private fun getSetCallsLog() {
-        textView.visibility = View.GONE
-        callsLogsArray.clear()
-        val projection = arrayOf(
-            CallLog.Calls.CACHED_NAME,
-            CallLog.Calls.NUMBER,
-            CallLog.Calls.TYPE,
-            CallLog.Calls.DATE
-        )
-        val cursor: Cursor? = activity?.applicationContext!!.contentResolver
-            .query(CallLog.Calls.CONTENT_URI, projection, null, null, null)
-        if (cursor != null) {
-            val currentTime = Calendar.getInstance().timeInMillis
-            while (cursor.moveToNext()) {
-                val name = cursor.getString(0)
-                if (name == ""){
-                    val number = cursor.getString(1)
-                    val type = cursor.getString(2)
-                    val time = cursor.getString(3)
-
-                    val callType = getcallCodes(type.toInt())
-                    val duration = timeDuration(currentTime - time.toLong())
-                    val log = CallData(number, callType, duration)
-                    callsLogsArray.add(log)
-                }
-            }
-        }
-        cursor?.close()
-        recyclerView.adapter?.notifyDataSetChanged()
-    }
-
-    private fun timeDuration(long: Long): String {
-        val seconds  = long/1000
-        val minutes = seconds/60
-        val hours = minutes/60
-        val days = hours/24
-        val weeks = days/7
-        val months = days/30
-        val years = months/12
-        val timelist = listOf(years.toInt(),months.toInt(),weeks.toInt(),days.toInt(),hours.toInt(),minutes.toInt(),seconds.toInt())
-        val abblist = listOf("y","mo","w","d","h","m","s")
-        for (i in timelist.indices){
-            if (timelist[i]>=1){
-                return "${timelist[i]}${abblist[i]} ago"
-            }
-        }
-        return ""
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_call_logs, container, false)
-    }
 }
--- app/src/main/java/com/supersuman/whysave/CallLogsRecyclerViewAdapter.kt
+++ app/src/main/java/com/supersuman/whysave/CallLogsRecyclerViewAdapter.kt
@@ -6,55 +6,81 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.card.MaterialCardView
-import com.google.android.material.textfield.TextInputEditText
+import com.supersuman.whysave.databinding.EachCallLogBinding
+import com.wickerlabs.logmanager.LogObject
+import java.util.Calendar
 
-class CallLogsRecyclerViewAdapter(
-    private val mutableList: MutableList<CallData>,
-    private val phonenumberEditText: TextInputEditText
-) : RecyclerView.Adapter<CallLogsRecyclerViewAdapter.ViewHolder>() {
-    class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
-        val context: Context = view.context
-        val number : TextView = view.findViewById(R.id.eachPhoneNumber)
-        val type : TextView = view.findViewById(R.id.eachCallType)
-        val time : TextView = view.findViewById(R.id.eachCallTime)
-        val materialCardView : MaterialCardView = view.findViewById(R.id.eachCallLogCard)
-    }
+class CallLogsRecyclerViewAdapter(private val mutableList: MutableList<LogObject>?) :
+    RecyclerView.Adapter<CallLogsRecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: EachCallLogBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.each_call_log,parent,false))
+        val binding = EachCallLogBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.number.text = mutableList[position].number
-        holder.type.text = mutableList[position].type
-        holder.time.text = mutableList[position].duration
-        holder.materialCardView.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW,
-                Uri.parse("https://api.whatsapp.com/send/?phone=%2B${mutableList[position].number}&text&app_absent=0"))
-            holder.context.startActivity(browserIntent)
+        if (mutableList == null) return
+        holder.binding.eachPhoneNumber.text = mutableList[position].contactName
+        holder.binding.eachCallType.text = getcallType(mutableList[position].type)
+        holder.binding.eachCallTime.text = timeDuration(mutableList[position].date)
+        holder.binding.eachCallLogCard.setOnClickListener {
+            val browserIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://api.whatsapp.com/send/?phone=%2B${mutableList[position].number}&text&app_absent=0")
+            )
+            holder.binding.root.context.startActivity(browserIntent)
         }
-        holder.materialCardView.setOnLongClickListener {
-            copyToClipboard(holder.context, mutableList[position].number)
-            phonenumberEditText.setText(mutableList[position].number)
+        holder.binding.eachCallLogCard.setOnLongClickListener {
+            copyToClipboard(holder.binding.root.context, mutableList[position].number)
             return@setOnLongClickListener true
         }
     }
 
-    override fun getItemCount(): Int {
-        return mutableList.size
-    }
+    override fun getItemCount(): Int { return mutableList?.size ?: 0 }
 
     private fun copyToClipboard(context: Context, text: String) {
         val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clip = ClipData.newPlainText("Phonenumber",text)
+        val clip = ClipData.newPlainText("Phonenumber", text)
         clipboard.setPrimaryClip(clip)
-        Toast.makeText(context,"Phonenumber copied",Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "Phonenumber copied", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getcallType(i: Int): String {
+        when (i) {
+            1 -> return "Incoming"
+            2 -> return "Outgoing"
+            3 -> return "Missed"
+            4 -> return "Voicemail"
+            5 -> return "Declined"
+            6 -> return "Blocked"
+            7 -> return "Answered Externally"
+        }
+        return ""
+    }
+
+    private fun timeDuration(long: Long): String {
+        val cal  = Calendar.getInstance().timeInMillis
+        val diff = cal - long
+        val seconds  = diff/1000
+        val minutes = seconds/60
+        val hours = minutes/60
+        val days = hours/24
+        val weeks = days/7
+        val months = days/30
+        val years = months/12
+        val timelist = listOf(years,months,weeks,days,hours,minutes,seconds)
+        val abblist = listOf("y","mo","w","d","h","m","s")
+        for (i in timelist.indices){
+            if (timelist[i].toInt()>=1){
+                return "${timelist[i].toInt()}${abblist[i]} ago"
+            }
+        }
+        return ""
     }
 
 }
